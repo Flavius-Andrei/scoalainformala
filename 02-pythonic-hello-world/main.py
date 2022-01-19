2f749b872cf58a6c8ec3141e51462ae837a38c10
unknown <andyflv99@yahoo.ro>
2022-01-19 21:28:40 +0200

    curs02

---
+++ 02-pythonic-hello-world/main.py
@@ -0,0 +1,60 @@
+# print("Hello World")
+# print(1,type(1)) # tipul int
+# print(1.1, type(1.1)) # type float (rational / zecimal)
+# print(1j, type(1j)) #type complex
+# print(int(1.1))
+# print(float(1))
+# print(complex(1))
+# print(1+1) # adunare
+# print(1-1) # scadere
+# print(1*3) # inmultire
+# print(5 / 2) # impartire
+# print(5 // 2) # catul
+# print(5 % 2) #restul => modulo
+# print(5 ** 2) # ridicarea la putere
+
+# print( 5== 2) # operator de egailtate => returneaza un boolean (True 1 sau False 0)
+# print(5 != 2) # operator de inegalitate => returneaza un boolean (True 1 sau False 0)
+# print(5 > 2)
+# print(5 < 2)
+# print(5 >= 2)
+# print(5 <= 2)
+# print(5 > 2 and 3 > 1) # operator logic si
+# print(5 > 2 or 3 > 1) # operator logic sau
+# print(not(5 > 2 and 3 > 1)) # operator de negare
+
+#print(7 is 7)
+#print(7 is not 7)
+
+
+# my_var = 0
+# my_var, my_var_1 = 0,1
+# my_var = 3
+# my_var = my_var + 1 # incrementare
+# my_var += 1
+# my_var *= 2 # asignarea prin inmultire
+# print(my_var)
+
+# print("Ana are mere")
+# print('Ana are mere')
+
+# varibila = "Ana 'are' mere'
+# varibila = 'Ana "are" mere'
+# varibila  = 'Ana \'are\' \mere'
+# variabila = """ Ana are
+# mere
+# """
+# nume = "Trifu"
+# prenume = "Flavius"
+# varsta = 19
+# inaltime = 1.90
+# prezentare = "Numele meu este {0} si prenumele este {1} ".format(nume, prenume)
+# prezentare = f"Numele meu este {nume} si prenumele meu este {prenume}"
+# prezentare = "Numele meu este " + nume + "si prenumele meu este" + prenume + " si varsta mea sete" + str(varsta) + "."
+# prezentare = f"Numele meu este  {nume} si prenumele meu este {prenume} si varsta este {varsta} "
+# print(prezentare)
+# calcul = inaltime + varsta
+# calcul = inaltime + varsta
+# print(calcul)
+
+
